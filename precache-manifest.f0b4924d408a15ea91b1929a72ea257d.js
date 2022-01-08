97900c11834075f03928e1ea32b4ed58dd31aeff
0101dd <90738703+0101dd@users.noreply.github.com>
2022-01-08 01:52:36 +0000

    Deploying to gh-pages from @ 0101dd/TOMATO@03f6fc23596b7193bf0297a54133d8d81b514b4d 🚀

---
+++ precache-manifest.f0b4924d408a15ea91b1929a72ea257d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0d1e31dfbe96f301026",
+    "url": "css/app.c7766dc6.css"
+  },
+  {
+    "revision": "f5da53633b039a305ede",
+    "url": "css/chunk-vendors.989680b1.css"
+  },
+  {
+    "revision": "72ede87dd57015ac34722d462e2d9fcf",
+    "url": "img/tomato.png"
+  },
+  {
+    "revision": "d661a46367f640a657e9e3f217e8e23e",
+    "url": "index.html"
+  },
+  {
+    "revision": "a0d1e31dfbe96f301026",
+    "url": "js/app.77e0f4d4.js"
+  },
+  {
+    "revision": "f5da53633b039a305ede",
+    "url": "js/chunk-vendors.c7f154da.js"
+  },
+  {
+    "revision": "906e02dd75331e0ebd30",
+    "url": "js/list.03092d79.js"
+  },
+  {
+    "revision": "8c4b8bbe27bde70ed06a",
+    "url": "js/notfound.082bad65.js"
+  },
+  {
+    "revision": "d4b8b991af843073633d8e7cf2dedc12",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
